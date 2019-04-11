24fc492aa8a027171a4b1bc3dcb4cf75937fb771
Jean-Kenel Dessources <jkenel_23@yahoo.fr>
2019-04-11 13:52:55 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -38,8 +38,13 @@ def valid_move?(board, index)
 end
 
 
-def move(board, index, value = "X")
-  board[index] = value
+# def move(board, index, value = "X")
+#   board[index] = value
+#   return board
+# end
+
+def move(board, index, character = "X")
+  board[index] = character
   return board
 end
 
