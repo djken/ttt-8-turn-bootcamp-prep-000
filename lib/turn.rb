e4ca55d9281b8debd5d0166fcfe10e3ab1b3bed7
Jean-Kenel Dessources <jkenel_23@yahoo.fr>
2019-04-11 13:56:08 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -36,7 +36,7 @@ def valid_move?(board, index)
   #   return false
   # end  
   
-  if (position_taken?(board, index)) == false && (on_board?(index) == true)
+  if (position_taken?(board, position)) == false && (on_board?(position) == true)
     return true
   else
     return false
